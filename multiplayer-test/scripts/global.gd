dc8f9e365282e2be02cc9ee42fbf9ce8ff31afc1
ElrohirGT <elrohirgt@gmail.com>
2026-08-20 17:45:53 -0600

    fix: RPC error and ball spawn error

--- multiplayer-test/scripts/global.gd
+++ multiplayer-test/scripts/global.gd
@@ -6,10 +6,10 @@ var username: String
 
 const BALL = preload("uid://cww8t5m62gx6p")
 
-@rpc("any_peer")
+@rpc("any_peer", "call_local")
 func shoot_ball(position: Vector3, dir: Vector3, force: float):
 	var new_ball: RigidBody3D = BALL.instantiate()
-	new_ball.global_position = position + Vector3(0, -0.5, 0) + (dir*2.0)
+	new_ball.position = position + Vector3(0, -0.5, 0) + (dir*2.0)
 	var force_vec = dir*force
 	spawn_container.add_child(new_ball, true)
 	new_ball.apply_central_force(force_vec)
